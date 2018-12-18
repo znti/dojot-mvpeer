cf1c2c15e1b6cb6566cdff63007aa41f878416a8
Zanetti <znti.rafael@gmail.com>
2018-12-18 16:52:49 -0200

    Add device messaging basic structure

--- src/Device.jsx
+++ src/Device.jsx
@@ -4,13 +4,40 @@ export default class Device extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {...props};
+		let deviceId = props.data.id;
+		let message = {message: `hi from ${deviceId}`};
+
+		this.state = {
+			deviceId,
+			message,
+		}
+	}
+
+	handleChange = (e) => {
+		let message = e.target.value;
+		console.log(`[${this.state.deviceId}] - message set as`, message);
+		this.setState({message});
+	}
+
+	handleSend = (e) => {
+		let {deviceId, message} = this.state;
+		console.log(`Sending ${JSON.stringify(message)} from ${deviceId}`);
+		
 	}
 
 	render() {
-		let deviceId = this.state.data.id;
+		let deviceId = this.state.deviceId;
+		let defaultVal = this.state.message; //{message: `hi from ${deviceId}`};
 		return (
-			<h2>{`Device ${deviceId}`}</h2>
+			<div>
+				<h2>{`Device ${deviceId}`}</h2>
+				<textarea
+					defaultValue={JSON.stringify(defaultVal)}
+					onChange={this.handleChange}
+					>
+				</textarea>
+				<input type="button" value="Send" onClick={this.handleSend}/>
+			</div>
 		);
 	}
 
